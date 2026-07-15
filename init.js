dc9c90eb0916189e6f004951ac971eae3d2c427a
asafonov <me@asafonov.org>
2026-07-15 11:38:11 +0000

    typo fix

--- init.js
+++ init.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  const test = v => {
+  const test = async v => {
     const totp = new TOTP()
     const url = v.data
     alert(`url ${url}`)
     const parsedData = totp.parseData(url)
     alert(`data ${JSON.stringify(parsedData)}`)
-    const otp = totp.generateTOTP(parsedData.secret)
+    const otp = await totp.generateTOTP(parsedData.secret)
     alert(`otp ${otp}`)
   }
 
